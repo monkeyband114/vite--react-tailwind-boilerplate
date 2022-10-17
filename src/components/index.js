9abc162c8a4996a675b1e53eacc5eeaa3c08d853
Michael Voke <gadocer54@gmail.com>
2022-10-17 09:30:19 +0100

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,24 @@
+import Client from './Client';
+import Business from './Business';
+import Billing from './Billing';
+import CardDeal from './CardDeal';
+import Navbar from './Navbar';
+import CTA from './CTA';
+import Stats from './Stats';
+import Footer from './Footer';
+import Testimonial from './Testimonial';
+import Hero from './Hero';
+
+
+export {
+    Navbar,
+    Billing,
+    CardDeal,
+    Client,
+    Business,
+    Stats,
+    CTA,
+    Footer,
+    Testimonial,
+    Hero
+}
